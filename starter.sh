7878058ce9a18d068664b8dc40725970b7fa3e7a
Mohammed Shafeer <abuabdullah.sed@gmail.com>
2021-01-05 23:50:15 +0530

    added deploy script

---
+++ starter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Set project folder as current directory"
+cd ./
+
+echo "Stash the un committed files"
+git stash save "tbd"
+
+echo "Pull latest changes"
+git pull origin master
+
+echo "Stoping the App"
+sudo pm2 stop "Quran-gql"
+
+echo "Deleting the App"
+sudo pm2 delete "Quran-gql"
+
+echo "Building the App"
+npm run build
+
+echo "Starting the App"
+sudo pm2 start npm --no-automation --name Quran-gql -- run "start:prod"
